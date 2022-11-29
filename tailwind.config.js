5f9792bad40ab16f39357ade1c600156b32acba5
JunjPan <elisepjj@hotmail.com>
2022-11-28 17:11:41 -0800

    add tailwind CSS and configure file by running npx tailwindcss init -p

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config}
+ * https://tailwindcss.com/docs/guides/nextjs
+ */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
